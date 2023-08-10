38f89ba604e313fb848bb4a8f7cf36def7b82238
zhangchao162 <1623804006@qq.com>
2023-08-10 12:50:52 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,39 @@
 # @File    : setup.py
 # @Software: PyCharm
 # @Email   : zhangchao5@genomics.cn
-from setuptools import setup, find_packages
+import setuptools
+from wheel.bdist_wheel import bdist_wheel
 
 __version__ = "1.0.0"
 
+class BDistWheel(bdist_wheel):
+    def get_tag(self):
+        return (self.python_tag, "none", "linux_X86_64")
+
+cmdclass = {
+    "bdist_wheel": BDistWheel,
+}
+
 requirements = open("requirements.txt").readline()
 
-setup(
+setuptools.setup(
     name="BatchQC",
     version=__version__,
     author="zhangchao",
     author_email="zhangchao5@genomics.cn",
     url="https://github.com/BGIResearch/batchqc-pipeline.git",
-    description="BatchQC Pipeline",
-    python_requires=">=3.8.8",
-    packages=find_packages(),
-    install_requires=requirements
+    description="BatchEval",
+    python_requires=">=3.8",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    cmdclass=cmdclass,
+    package_data={'': ["*.html"]},
 )
 
 
